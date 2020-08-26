b890c4decc54cf92c90b7cb2cf63920265cfc6d7
Roman <romanjeanelie@yahoo.fr>
2020-08-26 18:37:59 +0200

    add caillou transition

--- src/Components/Cailloux.js
+++ src/Components/Cailloux.js
@@ -19,6 +19,24 @@ const Cailloux = ({ className }) => {
       transition: { duration: 2 },
     },
   };
+
+  const svgVariants = {
+    hidden: { transform: "scale(0.15)" },
+    visible: {
+      transform: "scale(0.15)",
+      transition: { delay: 0.4, duration: 1.7 },
+    },
+  };
+
+  const pathVariants = {
+    hidden: { opacity: 0.7, pathLength: 0 },
+    visible: {
+      opacity: 0.7,
+      pathLength: 1,
+      transition: { duration: 40, ease: "easeInOut" },
+    },
+  };
+
   return (
     <div className={className}>
       cailloux
@@ -40,6 +58,25 @@ const Cailloux = ({ className }) => {
             et il y a de la place pour jouer
           </p>
         </motion.div>
+        <motion.svg
+          variants={svgVariants}
+          initial="hidden"
+          animate="visible"
+          className="caillou"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 63.8651 51.914"
+        >
+          <g id="Layer_2" data-name="Layer 2">
+            <g id="Layer_1-2" data-name="Layer 1-2">
+              <motion.path
+                variants={pathVariants}
+                class="cls-1"
+                d="M3.3913,18.49C.8833,23.2136-.4716,28.8946,1.3161,33.9354c1.6568,4.6719,5.7292,8.0728,10,10.59A50.6126,50.6126,0,0,0,48.7285,49.99c4.9021-1.2126,9.9256-3.423,12.5972-7.7083,3.1483-5.05,2.2177-11.6489.1261-17.22C57.2864,13.9664,48.2237,1.6734,35.9818.6106,23.0132-.5152,9.4612,7.0569,3.3913,18.49Z"
+                transform="translate(0 0)"
+              />
+            </g>
+          </g>
+        </motion.svg>
       </AnimatePresence>
     </div>
   );
@@ -59,5 +96,13 @@ export default styled(Cailloux)`
     width: 90%;
     margin-top: 50px;
     text-align: center;
+  }
+  .caillou {
+    fill: none;
+    stroke: #f6f6f6;
+    stroke-miterlimit: 10;
+    position: fixed;
+    margin: 0 auto;
+    bottom: 100px;
   }
 `;
